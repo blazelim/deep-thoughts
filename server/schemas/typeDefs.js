9ee8a45ee78dcccac8b691660ce0e42ccffd02b1
Blaze Lim <blazebentleycolim@gmail.com>
2021-07-01 16:33:49 -0700

    added mutation routes

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,6 @@
 // import gpl tagged template function
 const { gql } = require('apollo-server-express')
 
-
-
 // create our typeDefs
 const typeDefs = gql`
     type Thought {
@@ -31,11 +29,25 @@ const typeDefs = gql`
         friends: [User]
     }
 
+    type Auth {
+        token: ID!
+        user: User
+    }
+
     type Query {
+        me: User
         users: [User]
         user(username: String!): User
         thoughts(username: String): [Thought]
         thought(_id: ID!): Thought
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addThought(thoughtText: String!): Thought
+        addReaction(thoughtId: ID!, reactionBody: String!): Thought
+        addFriend(friendId: ID!): User
     }
 `;
 
